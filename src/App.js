bc9e7ad1d05dd7622a5c8a48f04405eb7417094b
Jacob Gadikian <jacobgadikian@gmail.com>
2019-10-31 13:04:32 -0400

    working badly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+function Todo({ todo, index }) {
+  return (
+    <div className="todo">
+      { todo.text }
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todos, setTodos] = useState ([
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Eat Pickles',
+      isCompleted: false
+    },
+    {
+      text: 'I love pickles',
+      isCompleted: false
+    },
+]);
+
+return (
+  <div className="app">
+    <div className="todo-list">
+    {todos.map((todo, index) => (
+      <Todo key={index} index={index} todo={todo} />
+    ))}
     </div>
-  );
+    </div>
+)
 }
+
+
+
+
 
 export default App;
