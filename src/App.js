d063b99e3910d68110b55d56b21bf29ec55d669f
Jacob Gadikian <jacobgadikian@gmail.com>
2019-11-01 12:39:49 -0400

    New Todos now work

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ function Todo({ todo, index }) {
   )
 }
 
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+    )
+}
+
 function App() {
   const [todos, setTodos] = useState ([
     {
@@ -31,6 +48,7 @@ return (
     {todos.map((todo, index) => (
       <Todo key={index} index={index} todo={todo} />
     ))}
+    <TodoForm addTodo={addTodo} />
     </div>
     </div>
 )
