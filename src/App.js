108bd1d2f9eacd908b3bd2144d56ec90340e3006
Jacob Gadikian <jacobgadikian@gmail.com>
2019-11-05 13:15:26 -0500

    Crossed out lines now work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './App.css'
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="todo">
-      { todo.text }
+    <div style={{textDecoration: todo.isCompleted? 'line-through': ''}} className="todo">
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
     </div>
   )
 }
@@ -21,7 +24,7 @@ function TodoForm({addTodo}) {
 
   return (
     <form onSubmit={handleSubmit}>
-    <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    <input type="text" placeholder="add todo" className="input" value={value} onChange={e => setValue(e.target.value)} />
     </form>
     )
 }
@@ -47,11 +50,19 @@ const addTodo = text => {
   setTodos(newTodos);
 }
 
+
+const completeTodo = index => {
+  const newTodos = [...todos];
+  newTodos[index].isCompleted = true;
+  setTodos(newTodos);
+};
+
+
 return (
   <div className="app">
     <div className="todo-list">
     {todos.map((todo, index) => (
-      <Todo key={index} index={index} todo={todo} />
+      <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
     ))}
     <TodoForm addTodo={addTodo} />
     </div>
